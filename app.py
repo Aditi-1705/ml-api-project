7d5b25f702f620e00817ecaa3694fa8247369e18
Aditi Agasgekar <aditiagasgekar@gmail.com>
2026-05-04 20:20:54 +0000

    Final API with confidence

--- app.py
+++ app.py
@@ -3,33 +3,44 @@ from pydantic import BaseModel
 import joblib
 import numpy as np
 
-app = FastAPI()
 from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI()
+
+# Enable CORS (VERY IMPORTANT for frontend)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],   # allow all (fine for project/demo)
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Load trained model
 model = joblib.load("model.pkl")
 
-# Define request schema
+
+# Input format
 class InputData(BaseModel):
     features: list
+
 
 @app.get("/")
 def home():
     return {"message": "Breast Cancer Prediction API"}
 
+
 @app.post("/predict")
 def predict(data: InputData):
     features = data.features
 
+    # Prediction
     prediction = model.predict([features])[0]
+
+    # Probability (confidence)
     probability = model.predict_proba([features])[0][1]
 
+    # Result label
     result = "Malignant" if prediction == 1 else "Benign"
 
     return {
